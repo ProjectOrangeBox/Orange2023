a69f9018d5d40fcd3b311690ead8ba47e27c36c0
Don Myers <dmyers@turn14.com>
2023-09-11 12:11:42 -0400

    check if bootstrap file is present

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,7 +11,9 @@ require_once __ROOT__ . '/vendor/autoload.php';
 
 mergeEnv(__ROOT__.'/.env');
 
-require_once __ROOT__.'/app/bootstrap.php';
+if (file_exists(__ROOT__.'/app/bootstrap.php')) {
+    require_once __ROOT__.'/app/bootstrap.php';
+}
 
 /* send config into application */
 http(include __ROOT__ . '/app/config/config.php');
