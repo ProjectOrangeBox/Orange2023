baad41ba235237ecd60a6ac3216e13faada562bd
Don Myers <dmyers@turn14.com>
2023-10-23 20:44:07 -0400

    add copy asset feature

--- config/quickview.php
+++ config/quickview.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 return [
-    '406' => ['contentType' => 'json', 'charSet' => 'UTF-8', 'responseCode' => 406,'template'=>'<?php foreach ($errors as $e) { echo "<p>".$e."</p>"; }'],
+
+    /* merged content below */
+
+    '406' => ['contentType' => 'json', 'charSet' => 'UTF-8', 'responseCode' => 406, 'template' => '<?php foreach ($errors as $e) { echo "<p>".$e."</p>"; }'],
     '201' => ['contentType' => 'json', 'charSet' => 'UTF-8', 'responseCode' => 201, 'send' => true],
     '202' => ['contentType' => 'json', 'charSet' => 'UTF-8', 'responseCode' => 202, 'send' => true],
+
+    /* end merged contents */
+
 ];
