31bdf74f3c7fd058f69c87977b1e1e5fb12c1390
Don Myers <dmyers@turn14.com>
2025-01-07 20:18:35 -0500

    update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,25 +2,26 @@
 
 declare(strict_types=1);
 
-// setup the application ROOT
-// handy for mocking data instead of hardwired a directory location based on the file
-// you can just change __ROOT__ to something else then change it back for example
+// All directories are based off of this root path and
+// everything goes under this path for security and easier portability
 define('__ROOT__', realpath(__DIR__ . '/../'));
 
-// where is our www directory?
-define('__WWW__', realpath(__DIR__));
+// where is our public www directory?
+define('__WWW__', __ROOT__ . '/htdocs');
 
-// merge our .env with the system env
+// we now merge our system properties with our .env properties
+// these make it easier to have specific properties based on the system
+// you can make sure for example developers have different properties then say production
 $_ENV = array_replace_recursive($_ENV, parse_ini_file(__ROOT__ . '/.env', true, INI_SCANNER_TYPED));
 
-// load our start up configuration
+// now we load up our most basic start up configuration
 $config = include __ROOT__ . '/config/config.php';
 
-// our own personal bootstraping file which can also modify $config if necessary
+// call our own personal bootstraping file which can also modify $config if necessary
 require_once __ROOT__ . '/bootstrap.php';
 
-// composer auto loader
+// load the standard composer autoloader
 require_once __ROOT__ . '/vendor/autoload.php';
 
-/* send config into http application and away we go! */
+// send the basic config into our http application and away we go!
 \orange\framework\Application::http($config);
