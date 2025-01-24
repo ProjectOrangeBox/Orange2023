d98eca125b896132e43442c6477d35a9a27100fa
Don Myers <dmyers@turn14.com>
2025-01-24 07:27:54 -0500

    update

--- config/config.php
+++ config/config.php
@@ -5,7 +5,4 @@ return [
 
     // what directory contains the configuration files
     'config directory' => __ROOT__ . '/config',
-
-    // what file returns the service locator array?
-    'services' => __ROOT__ . '/config/services.php',
 ];
