04f8d37a8cebfa8edad20a2f424aed2aad435d09
Don Myers <dmyers@turn14.com>
2025-03-05 15:37:35 -0500

    update

--- slice/src/app.js
+++ slice/src/app.js
@@ -241,8 +241,6 @@ class App {
                             jsonObject = parent.getProperty(jsonObject, modelProperty);
                         }
 
-                        let 
-
                         parent.setProperty(parent.model, appProperty, jsonObject);
 
                         parent.rebind();
