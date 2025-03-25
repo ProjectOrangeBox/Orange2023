14056a2f3b775facb6777d34bb89bbe32eaba0e9
Don Myers <dmyers@turn14.com>
2025-03-25 11:24:09 -0400

    updates

--- slice/src/models/people.js
+++ slice/src/models/people.js
@@ -37,43 +37,54 @@ var people = {
             app.go({ element: this, app: arguments[1], ...app.getAttr(this) });
         },
         update(index) {
+            // save this for success
             people.currentIndex = index;
+            // make a copy of this so we are not working on a reference to the original record
             people.updateRecord = Object.assign({}, people.list[people.currentIndex]);
 
+            // do the other stuff on the DOM element
             app.onAttrs({ ...app.getAttr(this) });
         },
         updatedSuccess(app, args) {
             people.actions.clearValidation();
 
-            // trigger each property update
+            // trigger each property "update" so tinybind updates the list
             for (const key in people.updateRecord) {
                 people.list[people.currentIndex][key] = people.updateRecord[key];
             }
         },
         create() {
-            // defaults
+            // defaults - this could also be retrieved from a rest call
             people.createRecord = {
                 age: 18,
                 color: 6,
             }
 
+            // do the other stuff on the DOM element
             app.onAttrs({ ...app.getAttr(this) });
         },
         createdSuccess(app, args) {
             people.actions.clearValidation();
 
+            // put the returned primary id in the record for rests calls back to the server
             people.createRecord.id = args.json.id;
+            // find the human readable version of the color id (which is a integer foreign key on the server)
             people.createRecord.colorname = searchArrayOfObjects(people.colorDropDown, people.createRecord.color, 'id', 'name');
 
+            // push the record onto the list array and tinybind will update it
             people.list.push(people.createRecord);
         },
-        deleteSuccess(index) {
+        delete(index) {
+            // save this for success
             people.currentIndex = index;
-            people.deleteRecord = Object.assign({}, people.list[people.currentIndex]);
+            // put a copy in the delete views record object
+            people.deleteRecord = people.list[people.currentIndex];
 
+            // do the other stuff on the DOM element
             app.onAttrs({ ...app.getAttr(this) });
         },
-        deleted(app, args) {
+        deletedSuccess() {
+            // pull the records from the list view array
             people.list.splice(people.currentIndex, 1);
         },
         redirect(url) {
