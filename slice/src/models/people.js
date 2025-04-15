9444bfc6acd2c40d669e1492ee1ece7dc20a6dde
Don Myers <dmyers@turn14.com>
2025-04-15 09:53:15 -0400

    updates

--- slice/src/models/people.js
+++ slice/src/models/people.js
@@ -49,19 +49,16 @@ var People = {
             People.currentIndex = index;
             // make a copy of this so we are not working on a reference to the original record
             People.updateRecord = Object.assign({}, People.list[People.currentIndex]);
-
             // do the other stuff on the DOM element
-            window['@app'].onAttrs({ ...window['@app'].getAttr(this) });
+            People.actions.doAttrsOn(this);
         },
         // on-success-action="actions.updatedSuccess"
         updatedSuccess(app, args) {
             // called from on-success-action
             People.actions.clearValidation();
-
             // find the color name in People.colorDropDown based on the color id in People.updateRecord.color
             People.updateRecord.colorname = searchArrayOfObjects(People.colorDropDown, People.updateRecord.color, 'id', 'name');
-
-            // trigger each property "update" so tinybind updates the list
+            // !important - copy each property which triggers an update in tinybind
             for (const key in People.updateRecord) {
                 People.list[People.currentIndex][key] = People.updateRecord[key];
             }
@@ -73,20 +70,17 @@ var People = {
                 age: 18,
                 color: 6,
             }
-
             // do the other stuff on the DOM element
-            window['@app'].onAttrs({ ...window['@app'].getAttr(this) });
+            People.actions.doAttrsOn(this);
         },
         // on-success-action="actions.createdSuccess"
         createdSuccess(app, args) {
             // called from on-success-action
             People.actions.clearValidation();
-
             // put the returned primary id in the record for rests calls back to the server
             People.createRecord.id = args.json.id;
             // find the human readable version of the color id (which is a integer foreign key on the server)
             People.createRecord.colorname = searchArrayOfObjects(People.colorDropDown, People.createRecord.color, 'id', 'name');
-
             // push the record onto the list array and tinybind will update it
             People.list.push(People.createRecord);
         },
@@ -96,9 +90,8 @@ var People = {
             People.currentIndex = index;
             // put a copy in the delete views record object
             People.deleteRecord = People.list[People.currentIndex];
-
             // do the other stuff on the DOM element
-            window['@app'].onAttrs({ ...window['@app'].getAttr(this) });
+            People.actions.doAttrsOn(this);
         },
         // on-success-action="actions.deletedSuccess"
         deletedSuccess() {
@@ -115,6 +108,9 @@ var People = {
             People.validation.invalid = {};
             People.validation.show = false;
             People.validation.array = [];
+        },
+        doAttrsOn(element) {
+            window['@app'].onAttrs({ ...window['@app'].getAttr(element) });
         },
     },
 };
